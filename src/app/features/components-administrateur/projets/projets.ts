fe205184a6987a35bb22291334afcc60ac48ecea
Deku5005 <dolooumar60@gmail.com>
2025-08-07 16:04:15 +0000

    Ajout de la page Projet Domaines Badges

--- src/app/features/components-administrateur/projets/projets.ts
+++ src/app/features/components-administrateur/projets/projets.ts
@@ -3,7 +3,7 @@ import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-projets',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './projets.html',
   styleUrl: './projets.css'
 })
