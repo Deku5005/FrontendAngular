c9a3b8a58e5857b1ab417bfc3abee31eca8ce8a7
Ibrahim BAH <bah49983@gmail.com>
2025-08-07 22:02:58 +0000

    features: mise en place complete de la fonctionnalité de tableau de bord du contributeur ainsi que ses petits details

--- src/app/features/coponents-contributeurs/list-projects/popup-eye/popup-eye.ts
+++ src/app/features/coponents-contributeurs/list-projects/popup-eye/popup-eye.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBookmark, faHandshake, faInfoCircle, faTags, faTasks, faTimes, faUserFriends } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-popup-eye',
@@ -11,40 +10,13 @@ import { faBookmark, faHandshake, faInfoCircle, faTags, faTasks, faTimes, faUser
 })
 export class PopupEye {
 
-  protected readonly faTimes= faTimes;
-  protected readonly faInfoCircle = faInfoCircle;
-  protected readonly faTasks= faTasks;
-  protected readonly faTags=faTags;
-  protected readonly faUserFriends=faUserFriends;
-  protected readonly faBookmark= faBookmark;
-  protected readonly faHandshake= faHandshake;
+  @Input() titreProjet: string = '';
+  @Input() niveau: string = '';
+  @Input() description: string = '';
 
-  // Inputs: données et état du modal
-  @Input() project: any = null;
-  @Input() showModal: boolean = false;
-  
-  // Outputs: événements de fermeture et d'action
   @Output() closeModal = new EventEmitter<void>();
-  @Output() saveProject = new EventEmitter<void>();
-  @Output() joinProject = new EventEmitter<void>();
-  
-  // Ferme le popup
-  onClose() {
+
+  fermerPopup() {
     this.closeModal.emit();
-  }
-  
-  // Enregistre le projet
-  onSave() {
-    this.saveProject.emit();
-  }
-  
-  // Rejoint le projet
-  onJoin() {
-    this.joinProject.emit();
-  }
-  
-  // Empêche la fermeture quand on clique à l'intérieur
-  preventClose(event: Event) {
-    event.stopPropagation();
   }
 }
