1654cf3525de03d986cc08584ecf02adfd0dd265
Mamoutou02 <sangaremoutou.360@.com>
2025-08-08 11:29:05 +0000

    Service de la page inscription  ok

--- src/app/features/components-administrateur/header/header.ts
+++ src/app/features/components-administrateur/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {faUserCircle} from '@fortawesome/free-solid-svg-icons';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
-    FaIconComponent
+    FaIconComponent,RouterOutlet
   ],
   templateUrl: './header.html',
   styleUrl: './header.css'
